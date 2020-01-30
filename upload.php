5a64a5bba6afbd661a6e3ad6ac95a4d558035efd
Yasoii <60357287+Yasoii@users.noreply.github.com>
2020-01-30 11:44:29 +0200

    css and uploud file creation
    image folder created

--- upload.php
+++ upload.php
@@ -1,18 +1,12 @@
-<?php 
-    $files = scandir('images');
-    $imageNames = array_slice($files, 2);
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Inspirational Movies</title>
+    <title>Upload Image</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-    
 </head>
 <body>
     <div class="container">
@@ -21,15 +15,16 @@
             <h2 class="heading">Inspire us with your 7 favorite inspirational movies for this week!</h2>
         </header>
 
-        <a class="upload-button" href="upload.php">Upload Image</a>
-
-        <div class="gallery">
-            <?php
-                foreach ($imageNames as $imageName) {
-                    echo "<img src=\"images/$imageName\" alt=\"Gallery Image\">";
-                }
-            ?>
-        </div>
+        <form action="postImage.php" method="post" enctype="multipart/form-data" class="upload-image-form">
+            <div>
+                <label>Upload Image: </label>
+                <input type="file" name="newImage">
+            </div>
+        
+            <div>
+                <input type="submit" value="Upload" class="upload-button">
+            </div>
+        </form>
     </div>
 </body>
 </html>
