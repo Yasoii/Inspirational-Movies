8a72749cf644eb8d05585b9122bb679decaeccfe
Yasoii <buyonline@abv.bg>
2020-01-31 01:37:48 +0200

    Dropdown

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
+    
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="style1.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 <style>
 body, html {
   height: 100%;
@@ -21,6 +24,20 @@ body, html {
   background-size: cover;
 }
 </style>
+<div class="navbar">
+  <a href="#home">Home</a>
+  <a href="#news">News</a>
+  <div class="dropdown">
+    <button class="dropbtn">Dropdown
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </div>
+</div>
 </head>
 <body>
 
